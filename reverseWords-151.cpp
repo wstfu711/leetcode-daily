20e61e7b6fcd62bac62255524715163d0f3f1161
wstfu <92413085+wstfu711@users.noreply.github.com>
2023-02-12 20:54:50 +0800

    Update reverseWords-151.cpp

--- reverseWords-151.cpp
+++ reverseWords-151.cpp
@@ -5,23 +5,22 @@ public:
         reverse(s.begin(), s.end());
         stack<char> st;
         int check = 0;
+        s+=' ';
 
         for(int i = 0 ; i < s.length() ; ++i){
             if(s[i] != ' '){
-                st.push(s[i]);
-                if(check == 0){
-                    check++;
+                if(st.empty() and ans != ""){
+                    ans += " ";
                 }
+                st.push(s[i]);
             }
-            else if(s[i] == ' ' and check != 0){
-                for(int j = 0 ; j < st.size() ; ++j){
+            else if(s[i] == ' '){
+                while(!st.empty()){
                     ans += st.top();
                     st.pop();
-                    if(j == st.size() - 1){
-                        ans += ' ';
-                    }
                 }
             }
+            
         }
 
         return ans;
