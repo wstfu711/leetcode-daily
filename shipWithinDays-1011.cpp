832d1be9a456b10f2a3c9046f4f5f796a417de16
wstfu <92413085+wstfu711@users.noreply.github.com>
2023-02-22 11:05:17 +0800

    Create shipWithinDays-1011.cpp

---
+++ shipWithinDays-1011.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int need = 1, cur = 0;
+            for(int weight : weights) {
+                if (cur + weight > mid) {
+                    need++;
+                    cur = 0;
+                }
+                cur += weight;
+            }
+            if (need <= days) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
