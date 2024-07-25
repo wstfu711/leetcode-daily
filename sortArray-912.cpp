3fc15f3b97f95876b8e0110c7d9f6fe7f0546998
wstfu <92413085+wstfu711@users.noreply.github.com>
2024-07-25 15:25:14 +0800

    Create sortArray-912.cpp

---
+++ sortArray-912.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        heapsort(nums);
+        return nums;
+    }
+
+    void heapsort(vector<int>& nums){
+        int n = nums.size();
+        for (int i = n / 2 - 1; i >= 0; i--){
+            heapify(nums, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+    }
+
+    void heapify(vector<int>& nums, int n, int i){
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && nums[left] > nums[largest]){
+            largest = left;
+        }
+        if (right < n && nums[right] > nums[largest]){
+            largest = right;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            heapify(nums, n, largest);
+        }
+    }
+
+    //heapsort實作
+    //不可使用內建的sort方法
+};
