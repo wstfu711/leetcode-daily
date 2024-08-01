7c172acaee5420ce1a208564d58fb59ead06e93e
wstfu <92413085+wstfu711@users.noreply.github.com>
2024-08-02 01:04:24 +0800

    Create countSeniors-2678.cpp

---
+++ countSeniors-2678.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for(int i = 0 ; i < details.size() ; i++){
+            if(details[i][11] > '6' || (details[i][11] == '6' && details[i][12] > '0')){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
