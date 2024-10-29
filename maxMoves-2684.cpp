3fdc30cac5458013a56a8a6e6513b59fde64c1cf
wstfu <92413085+wstfu711@users.noreply.github.com>
2024-10-29 23:14:54 +0800

    Create maxMoves-2684.cpp

---
+++ maxMoves-2684.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int col = n - 2; col >= 0; --col) {
+            for (int row = 0; row < m; ++row) {
+                if (row > 0 && grid[row - 1][col + 1] > grid[row][col]) {
+                    dp[row][col] = max(dp[row][col], dp[row - 1][col + 1] + 1);
+                }
+                if (grid[row][col + 1] > grid[row][col]) {
+                    dp[row][col] = max(dp[row][col], dp[row][col + 1] + 1);
+                }
+                if (row < m - 1 && grid[row + 1][col + 1] > grid[row][col]) {
+                    dp[row][col] = max(dp[row][col], dp[row + 1][col + 1] + 1);
+                }
+            }
+        }
+        int maxMoves = 0;
+        for (int row = 0; row < m; ++row) {
+            maxMoves = max(maxMoves, dp[row][0]);
+        }
+        return maxMoves;
+    }
+};
